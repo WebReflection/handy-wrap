5bff08dee2d662b1a907bfc5e8a962c3d4afb791
webreflection <andrea.giammarchi@gmail.com>
2023-01-31 22:37:43 +0100

    better types

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,8 +1,6 @@
-type Wrap<T> = EventTarget & {
-  [P in keyof T]?: T[P];
-} & {
-  emit(type:string, options?:object):Wrap<T>;
-  on(type:string, handler:function|EventListener, options?:object):Wrap<T>;
+type Wrap<T> = EventTarget & T & {
+  emit(type: string, options?: object): Wrap<T>;
+  on(type: string, handler: Function | EventListener, options?: object): Wrap<T>;
   valueOf():T;
 };
 
@@ -17,32 +15,32 @@ declare function $(css: string): Wrap<Element>;
  * @param css the *CSS* selector to query.
  * @param parent the `parentNode` to query.
  */
-declare function $(css: string, parent: Document | Element):Wrap<Element>;
+declare function $(css: string, parent: Document | Element): Wrap<Element>;
 
 /**
  * Exposes handy utilities to a single `EventTarget` entry.
  * @param target the signle `EventTarget` entry to wrap.
  */
-declare function $<T>(target: T):Wrap<T>;
+declare function $<T>(target: T): Wrap<T>;
 
 /**
  * Exposes handy utilities to a list of `EventTarget` entries.
  * @param css the *CSS* selector to query all.
  */
-declare function $$(css: string):Wrap<NodeList>;
+declare function $$(css: string): Wrap<NodeList>;
 
 /**
  * Exposes handy utilities to a list of `EventTarget` entries.
  * @param css the *CSS* selector to query all.
  * @param parent the `parentNode` to query.
  */
-declare function $$(css: string, parent: Document | Element):Wrap<NodeList>;
+declare function $$(css: string, parent: Document | Element): Wrap<NodeList>;
 
 
 /**
  * Exposes handy utilities to a list of `EventTarget` entries.
  * @param target the list of `EventTarget` entries to wrap.
  */
-declare function $$<T>(target: T):Wrap<T>;
+declare function $$<T>(target: T): Wrap<T>;
 
 export {$, $$};
